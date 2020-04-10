719a75a95278d9f8b526708bf4ef7dcf2c2f17c0
songyizhe <769724661@qq.com>
2020-04-10 14:48:51 +0800

    test

---
+++ env.js
@@ -0,0 +1,25 @@
+const configs = {
+  // 测试环境
+  test: {
+    API: 'http://test.api.counsel_terrace.tdianyi.com',
+  },
+
+  // 开发环境
+  development: {
+    API: 'http://test.api.counsel_terrace.tdianyi.com',
+
+  },
+
+  // 本地
+  local: {
+    API: 'http://test.api.counsel_terrace.tdianyi.com',
+    // API: 'http://192.168.2.107:80',
+  },
+
+  // 线上环境
+  master: {
+    API: 'http://platform-admin.api.tdianyi.com',
+  },
+};
+
+export default configs;
