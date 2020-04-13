a457297b3d980311576614d740fd79f98f170ee3
xiqoqilin <w3c_2014@163.com>
2020-04-13 14:02:21 +0800

    分类列表

--- src/pages/informationManagement/classifyToManage/classifyList/model.ts
+++ src/pages/informationManagement/classifyToManage/classifyList/model.ts
@@ -4,13 +4,15 @@ const Model = {
     classifyTitle: undefined, // 分类标题
     typeStatus: undefined, // 类型状态
     classifyCategory: undefined, // 所属分类
+    currentPage: 1, // 当前页
+    currentPageSize: 10, // 每页数量
 
-    HZCurrentPage: 1, // 当前页
-    HZCurrentPageSize: 10, // 每页数量
-    CKCurrentPage: 1, // 当前页
-    CKCurrentPageSize: 10, // 每页数量
+    // HZCurrentPage: 1, // 当前页
+    // HZCurrentPageSize: 10, // 每页数量
+    // CKCurrentPage: 1, // 当前页
+    // CKCurrentPageSize: 10, // 每页数量
 
-    tabKey: 1, // Tab切换值
+    // tabKey: 1, // Tab切换值
   },
   reducers: {
     setFussyForm(state: any, action: any) {
@@ -31,10 +33,17 @@ const Model = {
       };
     },
     // 设置Tab
-    setTabSelected(state: any, action: any) {
+    // setTabSelected(state: any, action: any) {
+    //   return {
+    //     ...state,
+    //     tabKey: action.payload.tabKey,
+    //   };
+    // },
+    setPaginationCurrent(state: any, action: any) {
       return {
         ...state,
-        tabKey: action.payload.tabKey,
+        currentPage: action.payload.currentPage,
+        currentPageSize: action.payload.currentPageSize,
       };
     },
   },
